a65acc83904c2789ae0d209f76a16c80f4f5448f
Rohith Reddy <rohith18151821@gmail.com>
2023-01-12 13:46:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
 });
 var corsOptions = {
-  origin: "https://z9btxo-3000.preview.csb.app",
+  origin: "https://z9btxo-3000.preview.csb.app,https://portfolio-ts-eight.vercel.app/",
 };
 app.use(cors(corsOptions));
 
